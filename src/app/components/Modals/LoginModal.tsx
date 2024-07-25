c07b3627e9b99e297943c064233004e197c0e808
DaniaMasnyi <danyamas07@gmail.com>
2024-07-25 22:42:51 +0300

    fixed few bugs

--- src/app/components/Modals/LoginModal.tsx
+++ src/app/components/Modals/LoginModal.tsx
@@ -1,128 +1,131 @@
-"use client";
+'use client';
 
-import axios from "axios";
-import { AiFillGithub } from "react-icons/ai";
-import { FcGoogle } from "react-icons/fc";
-import { signIn } from "next-auth/react";
-import { useCallback, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { signIn } from 'next-auth/react';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { AiFillGithub } from 'react-icons/ai';
 
-import useRegisterModal from "@/app/hooks/useRegisterModal";
-import Modal from "./Modal";
-import Heading from "../Heading";
-import Input from "../inputs/Input";
-import toast from "react-hot-toast";
-import Button from "../Button";
-import useLoginModal from "@/app/hooks/useLoginModal";
-import { useRouter } from "next/navigation";
+import useLoginModal from '@/app/hooks/useLoginModal';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+import Button from '../Button';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
+import Modal from './Modal';
 
 const LoginModal = () => {
-  const router = useRouter();
-  const registerModal = useRegisterModal(); // Виправлено назву змінної
-  const loginModal = useLoginModal();
-  const [isLoading, setIsLoading] = useState(false);
+	const router = useRouter();
+	const registerModal = useRegisterModal(); // Виправлено назву змінної
+	const loginModal = useLoginModal();
+	const [isLoading, setIsLoading] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FieldValues>({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FieldValues>({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	});
 
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    setIsLoading(true);
+	const onSubmit: SubmitHandler<FieldValues> = data => {
+		setIsLoading(true);
 
-    signIn("credentials", {
-      ...data,
-      redirect: false,
-    }).then((callback) => {
-      setIsLoading(false);
+		signIn('credentials', {
+			...data,
+			redirect: false,
+		}).then(callback => {
+			setIsLoading(false);
 
-      if (callback?.ok) {
-        toast.success("Logged In");
-        router.refresh();
-        loginModal.onClose();
-      }
+			if (callback?.ok) {
+				toast.success('Logged In');
+				router.refresh();
+				loginModal.onClose();
+			}
 
-      if (callback?.error) {
-        toast.error(callback.error);
-      }
-    });
-  };
+			if (callback?.error) {
+				toast.error(callback.error);
+			}
+		});
+	};
 
-  const bodyContent = (
-    <div className="flex flex-col gap-4">
-      <Heading title="Welcome back" subtitle="Login to your account" />
-      <Input
-        id="email"
-        label="Email"
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-        required
-      />
-      <Input
-        id="password"
-        type="password"
-        label="Password "
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-        required
-      />
-    </div>
-  );
+	const toggle = useCallback(() => {
+		loginModal.onClose();
+		registerModal.onOpen();
+	}, [loginModal, registerModal]);
 
-  const footerContent = (
-    <div className="flex flex-col gap-4 mt-3">
-      <hr />
-      <Button
-        outline
-        label="Continue with Github"
-        icon={AiFillGithub}
-        onClick={() => signIn("github")}
-      />
-      <div
-        className="
+	const bodyContent = (
+		<div className='flex flex-col gap-4'>
+			<Heading title='Welcome back' subtitle='Login to your account' />
+			<Input
+				id='email'
+				label='Email'
+				disabled={isLoading}
+				register={register}
+				errors={errors}
+				required
+			/>
+			<Input
+				id='password'
+				type='password'
+				label='Password '
+				disabled={isLoading}
+				register={register}
+				errors={errors}
+				required
+			/>
+		</div>
+	);
+
+	const footerContent = (
+		<div className='flex flex-col gap-4 mt-3'>
+			<hr />
+			<Button
+				outline
+				label='Continue with Github'
+				icon={AiFillGithub}
+				onClick={() => signIn('github')}
+			/>
+			<div
+				className='
       text-neutral-500
       text-center
       mt-4
       font-light
-      "
-      >
-        <div className="justify-center flex flex-row items-center gap-2">
-          <div>Already have an account?</div>
-          <div
-            onClick={registerModal.onClose}
-            className="
+      '
+			>
+				<div className='justify-center flex flex-row items-center gap-2'>
+					<div>First time using Airbnb?</div>
+					<div
+						onClick={toggle}
+						className='
           text-neutral-500
           cursor-pointer
           hover:underline
 
-          "
-          >
-            Log In
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-  return (
-    <Modal
-      disabled={isLoading}
-      isOpen={loginModal.isOpen} // Виправлено використання registerModal
-      title="Login"
-      actionLabel="Continue"
-      onClose={loginModal.onClose} // Виправлено використання registerModal
-      onSubmit={handleSubmit(onSubmit)}
-      body={bodyContent}
-      footer={footerContent}
-    />
-  );
+          '
+					>
+						Create an account
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+	return (
+		<Modal
+			disabled={isLoading}
+			isOpen={loginModal.isOpen} // Виправлено використання registerModal
+			title='Login'
+			actionLabel='Continue'
+			onClose={loginModal.onClose} // Виправлено використання registerModal
+			onSubmit={handleSubmit(onSubmit)}
+			body={bodyContent}
+			footer={footerContent}
+		/>
+	);
 };
 
 export default LoginModal;
