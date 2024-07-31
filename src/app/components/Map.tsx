9d11632d0f4b47c5c3f20a1248f1edc075ab46ae
DaniaMasnyi <danyamas07@gmail.com>
2024-07-31 17:22:19 +0300

    changes with leaflet

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,7 +1,32 @@
 'use client';
 
-const Map = () => {
-	return <div></div>;
+import L from 'leaflet';
+import { MapContainer } from 'react-leaflet';
+
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import marketShadow from 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/leaflet.css';
+
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+	iconUrl: markerIcon.src,
+	iconRetinaUrl: markerIcon.src,
+	shadowUrl: marketShadow.src,
+});
+
+interface MapProps {
+	center?: number[];
+}
+
+const Map: React.FC<MapProps> = ({ center }) => {
+	return;
+	<MapContainer
+		center={(center as L.LatLngExpression) || [51, -0.09]}
+		zoom={center ? 4 : 2}
+		scrollWheelZoom={false}
+		className='h-[35vh] rounded-lg'
+	></MapContainer>;
 };
 
 export default Map;
