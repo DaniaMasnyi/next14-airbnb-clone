2da412b59bbcf4328fe65255f7644d1f73709067
DaniaMasnyi <danyamas07@gmail.com>
2024-08-02 19:13:19 +0300

    listing card

--- src/app/components/listings/ListingCard.tsx
+++ src/app/components/listings/ListingCard.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import useCountries from '@/app/hooks/useCountries';
 import { SafeUser } from '@/app/types';
 import { Listing, Reservation } from '@prisma/client';
 import { useRouter } from 'next/navigation';
+import { useCallback, useMemo } from 'react';
+import { format } from 'date-fns';
+import Image from 'next/image';
 
 interface ListingCardProps {
 	data: Listing;
@@ -20,12 +24,79 @@ const ListingCard: React.FC<ListingCardProps> = ({
 	onAction,
 	disabled,
 	actionLabel,
-	actionId,
+	actionId = '',
 	currentUser,
 }) => {
 	const router = useRouter();
+	const { getByValue } = useCountries();
 
-	return <div></div>;
+	const location = getByValue(data.locationValue);
+
+	const handleClick = useCallback(
+		(e: React.MouseEvent<HTMLButtonElement>) => {
+			e.stopPropagation();
+
+			if (disabled) {
+				return;
+			}
+
+			onAction?.(actionId);
+		},
+		[onAction, actionId, disabled]
+	);
+
+	const price = useMemo(() => {
+		if (reservation) {
+			return reservation.totalPrice;
+		}
+
+		return data.price;
+	}, [reservation, data.price]);
+
+	const reservationDate = useMemo(() => {
+		if (!reservation) {
+			return null;
+		}
+
+		const start = new Date(reservation.startDate);
+		const end = new Date(reservation.endDate);
+
+		return `${format(start, 'PP')} - ${format(end, 'PP')}`;
+	}, [reservation]);
+
+	return (
+		<div
+			onClick={() => router.push(`/listing/${data.id}`)}
+			className='
+	col-span-1 cursor-pointer group
+	'
+		>
+			<div className='flex flex-col gap-2 w-full'>
+				<div
+					className='
+			aspect-square
+				w-full
+				relative
+				overflow-hidden
+				rounded-xl
+				'
+				>
+					<Image
+						fill
+						alt='Listing'
+						src={data.imageSrc}
+						className='
+object-cover
+h-full
+w-full
+group-hover:scale-110
+transition
+'
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ListingCard;
