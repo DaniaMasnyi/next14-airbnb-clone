926f15c8c05031714bf6cd770cf2cad516700381
DaniaMasnyi <danyamas07@gmail.com>
2024-08-03 21:42:19 +0300

    listing head

--- src/app/listings/[listingId]/ListingClient.tsx
+++ src/app/listings/[listingId]/ListingClient.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import Container from '@/app/components/Container';
+import ListingHead from '@/app/components/listings/ListingHead';
 import { categories } from '@/app/components/navbar/Categories';
 import { SafeListing, SafeUser } from '@/app/types';
 import { Reservation } from '@prisma/client';
@@ -19,7 +23,21 @@ const ListingClient: React.FC<ListingClientProps> = ({
 		return categories.find(item => item.label === listing.category);
 	}, [listing.category]);
 
-	return <div>Listing Client</div>;
+	return (
+		<Container>
+			<div className='max-w-screen-lg mx-auto'>
+				<div className='flex flex-col gap-6'>
+					<ListingHead
+						title={listing.title}
+						imageSrc={listing.imageSrc}
+						locationValue={listing.locationValue}
+						id={listing.id}
+						currentUser={currentUser}
+					/>
+				</div>
+			</div>
+		</Container>
+	);
 };
 
 export default ListingClient;
